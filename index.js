f04bca2c34d5b4c0a909db4f405f58a09796fec0
Sam Harrison <samuel.harrison@students.makeschool.com>
2018-12-13 13:20:16 -0800

    added basic API feature

---
+++ index.js
@@ -0,0 +1,60 @@
+var ctx;
+var img = new Image();
+var mousePressed = false;
+var lastX, lastY
+
+function start() {
+    ctx = document.getElementById('canvas').getContext('2d');
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/687px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg';
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    };
+    $('#canvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#canvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#canvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+        $('#canvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+moustache = document.getElementById('moustache')
+clear = document.getElementById('clear')
+
+moustache.addEventListener("click" ,function(){
+    ctx.beginPath();
+    ctx.moveTo(264, 300);
+    ctx.lineTo(304, 310);
+    ctx.lineTo(344, 300);
+    ctx.stroke();
+})
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        //ctx.strokeStyle = $('#selColor').val();
+        //ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+clear.addEventListener("click", function() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(img, 0, 0);
+})
